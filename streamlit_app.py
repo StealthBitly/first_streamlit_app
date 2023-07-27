826cac188c43a72ae6a2e483285fb4859b5067f3
StealthBitly <139898180+StealthBitly@users.noreply.github.com>
2023-07-26 18:30:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ streamlit.dataframe(my_data_rows)
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into fruits_selected ('" + add_my_fruit + "')")
+my_cur.execute("insert into fruits_selected values ('" + add_my_fruit + "')")
