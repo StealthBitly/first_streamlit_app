34eddf6da80c83a65117677ffa96f4d13600e4b6
StealthBitly <139898180+StealthBitly@users.noreply.github.com>
2023-07-26 20:06:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,14 +44,27 @@ except URLError as e:
 
 # don't run anything past here while we troubleshoot
 #streamlit.stop()
+#New Fruit Load List
+streamlit.header ("The fruit load list contains:") 
+#Snowflake-related functions
+def get_fruit_ load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load _list")
+    return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets ["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 #Fruit Load List
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
